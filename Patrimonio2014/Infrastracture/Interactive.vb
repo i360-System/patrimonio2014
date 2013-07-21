4664652a8be73adc83fea5b355c30ce943f22e70
alexanderma <infoedy@libero.it>
2013-07-21 19:42:53 +0200

    aggiunto metodo comune populate

--- Patrimonio2014/Infrastracture/Interactive.vb
+++ Patrimonio2014/Infrastracture/Interactive.vb
@@ -2,6 +2,14 @@
 
     Public Sub populate(ByRef frm As Form, ByVal rd As OleDb.OleDbDataReader)
 
+        For Each Control As TextBox In frm.Controls
+
+            While rd.Read
+                Control.Text = rd.Item(Control.Name.ToString)
+            End While
+
+        Next
+
     End Sub
 
 End Module
