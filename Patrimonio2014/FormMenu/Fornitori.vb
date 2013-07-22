ab042eee9e924947c652a8b06c675e9767de118d
dottortomas <cargenzio@libero.it>
2013-07-22 14:49:40 -0400

    ..

--- Patrimonio2014/FormMenu/Fornitori.vb
+++ Patrimonio2014/FormMenu/Fornitori.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Fornitori
 
     Private Sub ToolStripButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton1.Click
+        'controllo istanza
         Dim ric As New ToolBarComune
         Dim dr = ric.Ricerca(Me)
         If Not IsNothing(dr) Then
