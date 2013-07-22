77e750fba1d4c3eaceb31381ef37fcfb772e4154
dottortomas <cargenzio@libero.it>
2013-07-22 14:35:54 -0400

    .

--- Patrimonio2014/My Project/Settings.Designer.vb
+++ Patrimonio2014/My Project/Settings.Designer.vb
@@ -65,6 +65,16 @@ Namespace My
                 Me("StringaConnessione") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\archivi\IMMOBILI.accdb")>  _
+        Public ReadOnly Property IMMOBILIConnectionString() As String
+            Get
+                Return CType(Me("IMMOBILIConnectionString"),String)
+            End Get
+        End Property
     End Class
 End Namespace
 
