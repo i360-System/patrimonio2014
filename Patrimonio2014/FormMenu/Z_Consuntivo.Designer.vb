c757324f613c18e0be80eac072d86be8112f89e3
dottortomas <cargenzio@libero.it>
2013-07-25 11:32:30 -0400

    ..

---
+++ Patrimonio2014/FormMenu/Z_Consuntivo.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Z_Consuntivo
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ImmobileLabel As System.Windows.Forms.Label
+        Dim GestioneLabel As System.Windows.Forms.Label
+        Me.IMMOBILIDataSet = New Patrimonio2014.IMMOBILIDataSet()
+        Me.Z_ConsuntivoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Z_ConsuntivoTableAdapter = New Patrimonio2014.IMMOBILIDataSetTableAdapters.Z_ConsuntivoTableAdapter()
+        Me.TableAdapterManager = New Patrimonio2014.IMMOBILIDataSetTableAdapters.TableAdapterManager()
+        Me.ImmobileComboBox = New System.Windows.Forms.ComboBox()
+        Me.GestioneComboBox = New System.Windows.Forms.ComboBox()
+        Me.Z_ConsuntivoDettaglioBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Z_ConsuntivoDettaglioTableAdapter = New Patrimonio2014.IMMOBILIDataSetTableAdapters.Z_ConsuntivoDettaglioTableAdapter()
+        Me.Z_ConsuntivoDettaglioDataGridView = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn10 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        ImmobileLabel = New System.Windows.Forms.Label()
+        GestioneLabel = New System.Windows.Forms.Label()
+        CType(Me.IMMOBILIDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Z_ConsuntivoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Z_ConsuntivoDettaglioBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Z_ConsuntivoDettaglioDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'IMMOBILIDataSet
+        '
+        Me.IMMOBILIDataSet.DataSetName = "IMMOBILIDataSet"
+        Me.IMMOBILIDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'Z_ConsuntivoBindingSource
+        '
+        Me.Z_ConsuntivoBindingSource.DataMember = "Z_Consuntivo"
+        Me.Z_ConsuntivoBindingSource.DataSource = Me.IMMOBILIDataSet
+        '
+        'Z_ConsuntivoTableAdapter
+        '
+        Me.Z_ConsuntivoTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.AnagraficaStudioTableAdapter = Nothing
+        Me.TableAdapterManager.AnagraficheTableAdapter = Nothing
+        Me.TableAdapterManager.AssembleaDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.AssembleaTableAdapter = Nothing
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.ContiFinanziariTableAdapter = Nothing
+        Me.TableAdapterManager.CostantiDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.CostantiTableAdapter = Nothing
+        Me.TableAdapterManager.FabbricatiTableAdapter = Nothing
+        Me.TableAdapterManager.FondiTableAdapter = Nothing
+        Me.TableAdapterManager.FornitoriTableAdapter = Nothing
+        Me.TableAdapterManager.GruppiDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.GruppiTableAdapter = Nothing
+        Me.TableAdapterManager.ImmobiliTableAdapter = Nothing
+        Me.TableAdapterManager.OperatoriTableAdapter = Nothing
+        Me.TableAdapterManager.PianoContiConsuntivoDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.PianoContiConsuntivoTableAdapter = Nothing
+        Me.TableAdapterManager.PianoContiPreventivoDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.PianoContiPreventivoTableAdapter = Nothing
+        Me.TableAdapterManager.ScaleTableAdapter = Nothing
+        Me.TableAdapterManager.TabellaDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.TabellaTableAdapter = Nothing
+        Me.TableAdapterManager.UnitaDettaglioAnagraficaTableAdapter = Nothing
+        Me.TableAdapterManager.UnitaDettaglioLettureTableAdapter = Nothing
+        Me.TableAdapterManager.UnitaDettaglioMillesimiTableAdapter = Nothing
+        Me.TableAdapterManager.UnitaDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.UnitaTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = Patrimonio2014.IMMOBILIDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        Me.TableAdapterManager.V_ConsuntivoDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.V_ConsuntivoTableAdapter = Nothing
+        Me.TableAdapterManager.V_PreventivoDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.V_PreventivoTableAdapter = Nothing
+        Me.TableAdapterManager.W_EmissioniDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.W_EmissioniTableAdapter = Nothing
+        Me.TableAdapterManager.W_RitenuteTableAdapter = Nothing
+        Me.TableAdapterManager.W_TributiTableAdapter = Nothing
+        Me.TableAdapterManager.X_CausaliTableAdapter = Nothing
+        Me.TableAdapterManager.X_FattureDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.X_FattureTableAdapter = Nothing
+        Me.TableAdapterManager.X_MovimentiCondominiTableAdapter = Nothing
+        Me.TableAdapterManager.X_MovimentiFinanziariTableAdapter = Nothing
+        Me.TableAdapterManager.X_MovimentiFondiTableAdapter = Nothing
+        Me.TableAdapterManager.X_SpeseDescrizioniTableAdapter = Nothing
+        Me.TableAdapterManager.X_SpeseTableAdapter = Nothing
+        Me.TableAdapterManager.Y_GestioniDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.Y_GestioniTableAdapter = Nothing
+        Me.TableAdapterManager.Y_GestioniUnitaDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.Y_GestioniUnitaTableAdapter = Nothing
+        Me.TableAdapterManager.Z_AvvisiDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.Z_AvvisiTableAdapter = Nothing
+        Me.TableAdapterManager.Z_BilancioTableAdapter = Nothing
+        Me.TableAdapterManager.Z_ConsuntivoDettaglioTableAdapter = Me.Z_ConsuntivoDettaglioTableAdapter
+        Me.TableAdapterManager.Z_ConsuntivoRipartoDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.Z_ConsuntivoRipartoTableAdapter = Nothing
+        Me.TableAdapterManager.Z_ConsuntivoTableAdapter = Me.Z_ConsuntivoTableAdapter
+        Me.TableAdapterManager.Z_ConvocazioniDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.Z_ConvocazioniTableAdapter = Nothing
+        Me.TableAdapterManager.Z_EmissioniTableAdapter = Nothing
+        Me.TableAdapterManager.Z_EtichetteTableAdapter = Nothing
+        Me.TableAdapterManager.Z_InteressiTableAdapter = Nothing
+        Me.TableAdapterManager.Z_PreventivoRipartoDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.Z_PreventivoRipartoTableAdapter = Nothing
+        Me.TableAdapterManager.Z_RaffrontoTableAdapter = Nothing
+        Me.TableAdapterManager.Z_SaldoTableAdapter = Nothing
+        Me.TableAdapterManager.Z_TestoSollecitoTableAdapter = Nothing
+        Me.TableAdapterManager.ZZ_RipartoDettaglioTableAdapter = Nothing
+        Me.TableAdapterManager.ZZ_RipartoTableAdapter = Nothing
+        '
+        'ImmobileLabel
+        '
+        ImmobileLabel.AutoSize = True
+        ImmobileLabel.Location = New System.Drawing.Point(12, 59)
+        ImmobileLabel.Name = "ImmobileLabel"
+        ImmobileLabel.Size = New System.Drawing.Size(51, 13)
+        ImmobileLabel.TabIndex = 1
+        ImmobileLabel.Text = "Immobile:"
+        '
+        'ImmobileComboBox
+        '
+        Me.ImmobileComboBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Z_ConsuntivoBindingSource, "Immobile", True))
+        Me.ImmobileComboBox.FormattingEnabled = True
+        Me.ImmobileComboBox.Location = New System.Drawing.Point(15, 75)
+        Me.ImmobileComboBox.Name = "ImmobileComboBox"
+        Me.ImmobileComboBox.Size = New System.Drawing.Size(296, 21)
+        Me.ImmobileComboBox.TabIndex = 2
+        '
+        'GestioneLabel
+        '
+        GestioneLabel.AutoSize = True
+        GestioneLabel.Location = New System.Drawing.Point(317, 59)
+        GestioneLabel.Name = "GestioneLabel"
+        GestioneLabel.Size = New System.Drawing.Size(52, 13)
+        GestioneLabel.TabIndex = 2
+        GestioneLabel.Text = "Gestione:"
+        '
+        'GestioneComboBox
+        '
+        Me.GestioneComboBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Z_ConsuntivoBindingSource, "Gestione", True))
+        Me.GestioneComboBox.FormattingEnabled = True
+        Me.GestioneComboBox.Location = New System.Drawing.Point(320, 75)
+        Me.GestioneComboBox.Name = "GestioneComboBox"
+        Me.GestioneComboBox.Size = New System.Drawing.Size(292, 21)
+        Me.GestioneComboBox.TabIndex = 3
+        '
+        'Z_ConsuntivoDettaglioBindingSource
+        '
+        Me.Z_ConsuntivoDettaglioBindingSource.DataMember = "Z_ConsuntivoDettaglio"
+        Me.Z_ConsuntivoDettaglioBindingSource.DataSource = Me.IMMOBILIDataSet
+        '
+        'Z_ConsuntivoDettaglioTableAdapter
+        '
+        Me.Z_ConsuntivoDettaglioTableAdapter.ClearBeforeFill = True
+        '
+        'Z_ConsuntivoDettaglioDataGridView
+        '
+        Me.Z_ConsuntivoDettaglioDataGridView.AutoGenerateColumns = False
+        Me.Z_ConsuntivoDettaglioDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Z_ConsuntivoDettaglioDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn5, Me.DataGridViewTextBoxColumn6, Me.DataGridViewTextBoxColumn7, Me.DataGridViewTextBoxColumn8, Me.DataGridViewTextBoxColumn9, Me.DataGridViewTextBoxColumn10})
+        Me.Z_ConsuntivoDettaglioDataGridView.DataSource = Me.Z_ConsuntivoDettaglioBindingSource
+        Me.Z_ConsuntivoDettaglioDataGridView.Location = New System.Drawing.Point(16, 121)
+        Me.Z_ConsuntivoDettaglioDataGridView.Name = "Z_ConsuntivoDettaglioDataGridView"
+        Me.Z_ConsuntivoDettaglioDataGridView.Size = New System.Drawing.Size(786, 220)
+        Me.Z_ConsuntivoDettaglioDataGridView.TabIndex = 4
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "Gruppo"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Gruppo"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        Me.DataGridViewTextBoxColumn3.Width = 50
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "Spesa"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Spesa"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        Me.DataGridViewTextBoxColumn4.Width = 50
+        '
+        'DataGridViewTextBoxColumn5
+        '
+        Me.DataGridViewTextBoxColumn5.DataPropertyName = "Fabbricato"
+        Me.DataGridViewTextBoxColumn5.HeaderText = "Fabbricato"
+        Me.DataGridViewTextBoxColumn5.Name = "DataGridViewTextBoxColumn5"
+        Me.DataGridViewTextBoxColumn5.Width = 70
+        '
+        'DataGridViewTextBoxColumn6
+        '
+        Me.DataGridViewTextBoxColumn6.DataPropertyName = "Scala"
+        Me.DataGridViewTextBoxColumn6.HeaderText = "Scala"
+        Me.DataGridViewTextBoxColumn6.Name = "DataGridViewTextBoxColumn6"
+        Me.DataGridViewTextBoxColumn6.Width = 50
+        '
+        'DataGridViewTextBoxColumn7
+        '
+        Me.DataGridViewTextBoxColumn7.DataPropertyName = "Importo"
+        Me.DataGridViewTextBoxColumn7.HeaderText = "Importo"
+        Me.DataGridViewTextBoxColumn7.Name = "DataGridViewTextBoxColumn7"
+        '
+        'DataGridViewTextBoxColumn8
+        '
+        Me.DataGridViewTextBoxColumn8.DataPropertyName = "PianoConti"
+        Me.DataGridViewTextBoxColumn8.HeaderText = "PianoConti"
+        Me.DataGridViewTextBoxColumn8.Name = "DataGridViewTextBoxColumn8"
+        Me.DataGridViewTextBoxColumn8.Width = 70
+        '
+        'DataGridViewTextBoxColumn9
+        '
+        Me.DataGridViewTextBoxColumn9.DataPropertyName = "DescrizioneGruppo"
+        Me.DataGridViewTextBoxColumn9.HeaderText = "DescrizioneGruppo"
+        Me.DataGridViewTextBoxColumn9.Name = "DataGridViewTextBoxColumn9"
+        Me.DataGridViewTextBoxColumn9.Width = 150
+        '
+        'DataGridViewTextBoxColumn10
+        '
+        Me.DataGridViewTextBoxColumn10.DataPropertyName = "DescrizioneSpesa"
+        Me.DataGridViewTextBoxColumn10.HeaderText = "DescrizioneSpesa"
+        Me.DataGridViewTextBoxColumn10.Name = "DataGridViewTextBoxColumn10"
+        Me.DataGridViewTextBoxColumn10.Width = 150
+        '
+        'Z_Consuntivo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(823, 361)
+        Me.Controls.Add(Me.Z_ConsuntivoDettaglioDataGridView)
+        Me.Controls.Add(GestioneLabel)
+        Me.Controls.Add(Me.GestioneComboBox)
+        Me.Controls.Add(ImmobileLabel)
+        Me.Controls.Add(Me.ImmobileComboBox)
+        Me.Name = "Z_Consuntivo"
+        Me.Text = "Totali del Consuntivo"
+        CType(Me.IMMOBILIDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Z_ConsuntivoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Z_ConsuntivoDettaglioBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Z_ConsuntivoDettaglioDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents IMMOBILIDataSet As Patrimonio2014.IMMOBILIDataSet
+    Friend WithEvents Z_ConsuntivoBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents Z_ConsuntivoTableAdapter As Patrimonio2014.IMMOBILIDataSetTableAdapters.Z_ConsuntivoTableAdapter
+    Friend WithEvents TableAdapterManager As Patrimonio2014.IMMOBILIDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents Z_ConsuntivoDettaglioTableAdapter As Patrimonio2014.IMMOBILIDataSetTableAdapters.Z_ConsuntivoDettaglioTableAdapter
+    Friend WithEvents ImmobileComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents GestioneComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents Z_ConsuntivoDettaglioBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents Z_ConsuntivoDettaglioDataGridView As System.Windows.Forms.DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn5 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn6 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn7 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn8 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn9 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn10 As System.Windows.Forms.DataGridViewTextBoxColumn
+End Class
