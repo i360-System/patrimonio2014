b580c9219c9bed2ba0746a6a35234c0b8d25278b
dottortomas <cargenzio@libero.it>
2013-09-26 03:44:14 -0400

    .

--- Patrimonio2014/FormMenu/X_Spese.vb
+++ Patrimonio2014/FormMenu/X_Spese.vb
@@ -15,8 +15,14 @@ Public Class X_Spese
         'TODO: questa riga di codice carica i dati nella tabella 'IMMOBILIDataSet2.X_Spese'. È possibile spostarla o rimuoverla se necessario.
         Me.X_SpeseTableAdapter.Fill(Me.IMMOBILIDataSet2.X_Spese)
         CreaCombo("Immobili", "SELECT Immobile, (Immobile + ' - ' + Denominazione) AS DescrizioneImmobile FROM Immobili", ImmobileComboBox, "DescrizioneImmobile", "Immobile")
-        CreaCombo("Y_Gestioni", "SELECT Gestione, (CStr(Gestione) + ' - ' + Descrizione1) AS DescrizioneGestione FROM Y_Gestioni WHERE Immobile='" _
-                  & ImmobileComboBox.Text & "'", GestioneComboBox, "DescrizioneGestione", "Gestione")
+        CreaCombo("Y_Gestioni", "SELECT Gestione, (CStr(Gestione) + ' - ' + Descrizione1) AS DescrizioneGestione FROM Y_Gestioni WHERE Immobile='" & ImmobileComboBox.Text & "'", GestioneComboBox, "DescrizioneGestione", "Gestione")
+        CreaCombo("Fabbricati", "SELECT Fabbricato, (CStr(Fabbricato) + ' - ' + Descrizione) AS DescrizioneFabbricato FROM Fabbricati WHERE Immobile='" & ImmobileComboBox.Text & "'", FabbricatoComboBox, "DescrizioneFabbricato", "Fabbricato")
+        CreaCombo("FabbricatiDettaglio", "SELECT Scala, (CStr(Scala) + ' - ' + Descrizione) AS DescrizioneScala FROM FabbricatiDettaglio WHERE Immobile='" & ImmobileComboBox.Text & "' AND Fabbricato=" & FabbricatoComboBox.Text, "DescrizioneScala", "Scala")
+        CreaCombo("Unita", "SELECT Unita, (Unita + ' - ' + Nominativo1) AS DescrizioneUnita FROM Unita WHERE Immobile='" & ImmobileComboBox.Text & "'", UnitaComboBox, "DescrizioneUnita", "Unita")
+        'gruppo
+        'spesa
+        CreaCombo("Fornitori", "SELECT Fornitore, (Fornitore + ' - ' + Denominazione) AS DescrizioneFornitore FROM Fornitori", FornitoreComboBox, "DescrizioneFornitore", "Fornitore")
+        CreaCombo("W_Tributi", "SELECT Tributo, (CStr(Tributo) + ' - ' + Descrizione) AS DescrizioneTributo FROM W_Tributi", TrattamentoComboBox, "DescrizioneTributo", "Tributo")
 
     End Sub
     Private Sub CreaCombo(ByVal NomeTab As String, ByVal query As String, ByRef combo As ComboBox, DisplayMember As String, ValueMember As String)
@@ -48,6 +54,35 @@ Public Class X_Spese
 
     Private Sub GestioneComboBox_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles GestioneComboBox.SelectedIndexChanged
         GestioneComboBox.Text = GestioneComboBox.SelectedValue
+
+    End Sub
+
+    Private Sub CreaCombo(p1 As String, p2 As String, p3 As String, p4 As String)
+        Throw New NotImplementedException
+    End Sub
+
+    Private Sub FabbricatoComboBox_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles FabbricatoComboBox.SelectedIndexChanged
+        FabbricatoComboBox.Text = FabbricatoComboBox.SelectedValue
+
+    End Sub
+
+    Private Sub ScalaComboBox_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ScalaComboBox.SelectedIndexChanged
+        ScalaComboBox.Text = ScalaComboBox.SelectedValue
+
+    End Sub
+
+    Private Sub UnitaComboBox_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles UnitaComboBox.SelectedIndexChanged
+        UnitaComboBox.Text = UnitaComboBox.SelectedValue
+
+    End Sub
+
+    Private Sub FornitoreComboBox_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles FornitoreComboBox.SelectedIndexChanged
+        FornitoreComboBox.Text = FornitoreComboBox.SelectedValue
+
+    End Sub
+
+    Private Sub TrattamentoComboBox_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles TrattamentoComboBox.SelectedIndexChanged
+        TrattamentoComboBox.Text = TrattamentoComboBox.SelectedValue
 
     End Sub
 End Class
